21ee3cef56b99447eb8912aae0a393bfdb98af55
eemna <emnarobbana@gmail.com>
2025-11-02 19:03:01 +0100

    fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
-import rateLimiter from "./middleware/rateLimiter.js";
+//import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
 import usersRoute from "./routes/usersRoute.js";
 import suppliersRoute from "./routes/suppliersRoute.js";
@@ -11,13 +11,13 @@ import job from "./config/cron.js";
 dotenv.config();
 const app = express();
 if (process.env.NODE_ENV === "production") job.start();
-app.use(rateLimiter);
+//app.use(rateLimiter);
 // middleware
 app.use(express.json());
 const PORT = process.env.PORT || 5002;
 
 app.get("/api/health", (req, res) => {
-    res.status(200),json({ status: "ok"});
+    res.status(200).json({ status: "ok"});
 });
 
 app.get("/", (req, res) => 
