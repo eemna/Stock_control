c5dfcd262f830d762b0c0f5e83b301927eabc20f
eemna <emnarobbana@gmail.com>
2025-11-02 19:13:14 +0100

    Fix: corrected res.status().json syntax in /api/health

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
-//import rateLimiter from "./middleware/rateLimiter.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
 import usersRoute from "./routes/usersRoute.js";
 import suppliersRoute from "./routes/suppliersRoute.js";
@@ -11,7 +11,7 @@ import job from "./config/cron.js";
 dotenv.config();
 const app = express();
 if (process.env.NODE_ENV === "production") job.start();
-//app.use(rateLimiter);
+app.use(rateLimiter);
 // middleware
 app.use(express.json());
 const PORT = process.env.PORT || 5002;
